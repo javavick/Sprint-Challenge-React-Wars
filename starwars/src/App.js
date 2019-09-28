57fbab29c9b88e9de4e877ecb3154ead9a687325
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-09-27 22:00:10 -0400

    Add axios, get data of Star Wars characters from SWAPI, and add it to state

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  // Set Star Wars characters data to state
+  const [characters, setCharacters] = useState([]);
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  // Get data of Star Wars characters from SWAPI
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people")
+      .then((response) => {
+        console.log(response);
+        setCharacters(response.data.results);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
+  console.log(characters);
+
+  // App
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
     </div>
   );
-}
+};
 
 export default App;
